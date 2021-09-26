423e86b6928d99206bd382373de6b6856f6a1f97
Vikalp Rusia <53312141+vikalprusia@users.noreply.github.com>
2021-09-26 16:13:39 +0530

    use can now sign in
    Signed-off-by: vikalp Rusia
    Signed-off-by: vikalp Rusia <vikalprusia@gmail.com>

--- src/main/java/com/miniProject/controller/EntryController.java
+++ src/main/java/com/miniProject/controller/EntryController.java
@@ -4,7 +4,6 @@ import com.miniProject.DAO.PlayerDAO;
 import com.miniProject.entity.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -25,32 +24,17 @@ public class EntryController {
     }
 
     @GetMapping("/")
-    public String sample() {
+    public String entry() {
         return "entry";
-    }
-
-    @GetMapping("/register")
-    public String displayRegistrationForm(Model model) {
-        model.addAttribute("newPlayer", new Player());
-        return "registration-form";
     }
 
     @PostMapping("/process-form")
     public String processRegistrationForm(@Valid @ModelAttribute("newPlayer") Player newPlayer,
                                           BindingResult result) {
         if (result.hasErrors()) {
-            return "registration-form";
+            return "entry";
         } else {
             playerDAO.savePlayer(newPlayer);
-            return "redirect:log-in";
-        }
-    }
-
-    @GetMapping("/log-in")
-    public String logIn(HttpSession session) {
-        if (session.getAttribute("player") == null) {
-            return "log-in";
-        } else {
             return "home";
         }
     }
--- web/WEB-INF/view/entry.jsp
+++ web/WEB-INF/view/entry.jsp
@@ -22,15 +22,15 @@
 <div class="container">
     <div class="forms-container">
         <div class="signin-signup">
-            <form action="#" class="sign-in-form">
+            <form action="verify-log-in" method="post" class="sign-in-form">
                 <h2 class="title">Sign in</h2>
                 <div class="input-field">
                     <i class="fas fa-user"></i>
-                    <input type="text" placeholder="Username">
+                    <input type="text" placeholder="Username" name="userName">
                 </div>
                 <div class="input-field">
                     <i class="fas fa-lock"></i>
-                    <input type="password" placeholder="Password">
+                    <input type="password" placeholder="Password" name="password">
                 </div>
                 <input type="submit" value="Login" class="btn solid"/>
                 <p class="social-text">Or Sign in with social platforms</p>
