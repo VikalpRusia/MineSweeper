311bd7b2ffa0a241f806c34ccfb24fad716631c3
Vikalp Rusia <53312141+vikalprusia@users.noreply.github.com>
2021-09-26 14:07:14 +0530

    entry controller
    Signed-off-by: vikalp Rusia
    Signed-off-by: vikalp Rusia <vikalprusia@gmail.com>

--- src/main/java/com/miniProject/controller/EntryController.java
+++ src/main/java/com/miniProject/controller/EntryController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -22,9 +25,8 @@ public class EntryController {
     }
 
     @GetMapping("/")
-    @ResponseBody
     public String sample() {
-        return playerDAO.getAllPlayer().toString();
+        return "entry";
     }
 
     @GetMapping("/register")
