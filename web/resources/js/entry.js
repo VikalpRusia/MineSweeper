f8d3af737e37c2932b9236b90916b15d1c538944
vikalp Rusia <vikalprusia@gmail.com>
2021-09-26 22:27:52 +0530

    outline of js designed for validation
    Signed-off-by: vikalp Rusia <vikalprusia@gmail.com>

--- web/resources/js/entry.js
+++ web/resources/js/entry.js
@@ -9,3 +9,25 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+
+function validateSignUpForm() {
+  console.log("Starting validate sign up");
+  var form = document.getElementById("signUpForm");
+  if (form.userName.value == null || form.userName.value.trim() === "") {
+    alert("Cannot leave User Name empty");
+    form.userName.focus();
+    return false;
+  }
+  if (form.fullName.value == null || form.fullName.value.trim() === "") {
+    alert("Cannot leave Name empty");
+    form.fullName.focus();
+    return false;
+  }
+  if (form.password.value == null || form.password.value === "") {
+    alert("Cannot leave Password empty");
+    form.password.focus();
+    return false;
+  }
+  return true;
+}
